08f27f020fb3b1e019a84c0ab4cb293d9bcd82a7
Alexey Melezhik <melezhik@gmail.com>
2016-10-17 21:45:22 +0300

    new-project

--- cpanfile
+++ cpanfile
@@ -1,1 +1,1 @@
-requires 'Net::Ping';
+requires 'Net::Ping', '>= 2.51';
--- story.pl
+++ story.pl
@@ -3,6 +3,27 @@ use Net::Ping;
 my $host =  config()->{host};
 
 my $p = Net::Ping->new();
-print "<$host> is alive.\n" if $p->ping($host);
-$p->close();
+
+
+if ($p->ping($host)){
+  print "<$host> is alive (default ping).\n";
+  $p->close();
+  exit();
+}
+
+$p = Net::Ping->new('tcp',2);
+
+$p->port_number(scalar(getservbyname("http", "tcp")));
+
+while ($i++ <=4)
+{
+    if ($p->ping($host)){
+      print "<$host> is alive (http port).\n";
+      $p->close();
+      exit;
+    }
+    sleep(1);
+}
+undef($p);
+
  
